b9f4ee44c8019c9aa0a3b6562d1bab02de0af77a
luisgc93 <luisgc93@gmail.com>
2023-07-14 10:46:10 -0500

    filter schemaname in vacuum sql

--- vacuum.sql
+++ vacuum.sql
@@ -7,6 +7,7 @@ SELECT
   last_autovacuum,
   (n_dead_tup::float / NULLIF(n_live_tup, 0)) AS scale_factor
 FROM pg_stat_all_tables
+WHERE schemaname = 'public'
 ORDER BY n_dead_tup DESC, last_autovacuum ASC
 LIMIT 100;
 
