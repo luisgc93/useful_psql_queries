994f754951cc15add8b2ddd9a72500805d6dbaa4
luisgc93 <luisgc93@gmail.com>
2023-11-03 10:05:53 -0500

    improve stats query

--- query_stats.sql
+++ query_stats.sql
@@ -1,3 +1,4 @@
 select pid, application_name, now() - query_start as time, state, query
-from pg_catalog.pg_stat_activity 
+from pg_catalog.pg_stat_activity
+where query not in ('ROLLBACK;', 'ROLLBACK', 'COMMIT')
 order by time desc nulls last;
