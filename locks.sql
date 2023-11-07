5de4d137652b3337a7cac842d0da328bc3eaf2f6
luisgc93 <luisgc93@gmail.com>
2023-11-07 15:41:31 -0500

    improve locks query

--- locks.sql
+++ locks.sql
@@ -2,9 +2,12 @@ SELECT relname
 FROM pg_class
 WHERE oid in (pid1, pid2...);
 
-select pg_locks.pid, pg_stat_activity.application_name, pg_locks.mode, pg_class.relname, pg_stat_activity.state, pg_locks.granted, pg_stat_activity.query
-from pg_locks
-join pg_class on pg_locks.relation = pg_class.oid
-join pg_stat_activity on pg_stat_activity.pid = pg_locks.pid
-where pg_class.relname = 'table_name'
-order by granted asc;
+SELECT 
+pg_locks.pid, pg_stat_activity.application_name, pg_locks.mode, pg_class.relname, 
+pg_stat_activity.state, now() - pg_stat_activity.query_start AS time, pg_locks.granted, pg_stat_activity.query
+FROM pg_locks
+JOIN pg_class ON pg_locks.relation = pg_class.oid
+JOIN pg_stat_activity ON pg_stat_activity.pid = pg_locks.pid
+WHERE pg_class.relname = 'network'
+AND query NOT IN ('ROLLBACK;', 'ROLLBACK', 'COMMIT', 'SELECT')
+ORDER BY granted ASC;
