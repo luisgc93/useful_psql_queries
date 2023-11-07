4e299522a6945abe7c0d0970d3b741e9ac276f5b
luisgc93 <luisgc93@gmail.com>
2023-11-07 14:18:21 -0500

    add pg_locks query

--- locks.sql
+++ locks.sql
@@ -1,3 +1,8 @@
 SELECT relname
 FROM pg_class
 WHERE oid in (16714, 16843);
+
+select pg_locks.mode, pg_class.relname from pg_locks
+join pg_class on relation = oid
+where pg_class.relname = 'network'
+and pg_locks.mode != 'AccessShareLock' 
