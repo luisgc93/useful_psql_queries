68068316ad0fb634a40d3b87a772ac4b47c31057
luisgc93 <luisgc93@gmail.com>
2023-11-09 10:58:34 -0500

    add idle in transaction filter

--- query_stats.sql
+++ query_stats.sql
@@ -1,4 +1,5 @@
 select pid, application_name, now() - query_start as time, state, query
 from pg_catalog.pg_stat_activity
 where query not in ('ROLLBACK;', 'ROLLBACK', 'COMMIT')
-order by time desc nulls last;
+and state = 'idle in transaction'
+order by time desc nulls last
