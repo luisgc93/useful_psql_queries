c889da3ecc0416c2265bdbd59c2e4eb8fd4d47b3
luisgc93 <luisgc93@gmail.com>
2023-12-13 12:31:55 -0500

    add query for killing idle queries

--- locks.sql
+++ locks.sql
@@ -9,3 +9,17 @@ JOIN pg_stat_activity ON pg_stat_activity.pid = pg_locks.pid
 WHERE pg_class.relname = 'table_name'
 AND query NOT IN ('ROLLBACK;', 'ROLLBACK', 'COMMIT', 'SELECT')
 ORDER BY granted ASC;
+
+/* Terminate idle queries that are stopping the migration from acquiring the lock*/
+select pg_terminate_backend(pid) from 
+	(SELECT
+pg_locks.pid
+FROM pg_locks
+JOIN pg_class ON pg_locks.relation = pg_class.oid
+JOIN pg_stat_activity ON pg_stat_activity.pid = pg_locks.pid
+WHERE pg_class.relname = 'table_name'
+AND query NOT IN ('ROLLBACK;', 'ROLLBACK', 'COMMIT', 'SELECT')
+and state = 'idle in transaction'
+and pg_stat_activity.application_name in ('app_name')
+and now() - pg_stat_activity.query_start > '2 second'::interval
+) as pids;
