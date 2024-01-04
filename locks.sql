8de16e32f7e843fa62a94ac4667d74ee8c43af45
luisgc93 <luisgc93@gmail.com>
2024-01-04 10:31:57 +0100

    update locks query to use time in s

--- locks.sql
+++ locks.sql
@@ -2,13 +2,15 @@
 /* Fetch all locks for a table and some stats  */
 SELECT 
 pg_locks.pid, pg_stat_activity.application_name, pg_locks.mode, pg_class.relname, 
-pg_stat_activity.state, now() - pg_stat_activity.query_start AS time, pg_locks.granted, pg_stat_activity.query
+pg_stat_activity.state, 
+EXTRACT(epoch FROM (now() - pg_stat_activity.query_start)) AS time_in_seconds, 
+pg_locks.granted, pg_stat_activity.query
 FROM pg_locks
 JOIN pg_class ON pg_locks.relation = pg_class.oid
 JOIN pg_stat_activity ON pg_stat_activity.pid = pg_locks.pid
 WHERE pg_class.relname = 'table_name'
 AND query NOT IN ('ROLLBACK;', 'ROLLBACK', 'COMMIT', 'SELECT')
-ORDER BY granted ASC;
+ORDER BY time_in_seconds DESC;
 
 /* Terminate idle queries that are stopping the migration from acquiring the lock*/
 select pg_terminate_backend(pid) from 
